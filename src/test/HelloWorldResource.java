1b17192170c26b929fa18c9efa06ad6aa483ef2d
James Vu <jamesvu@eworldes.com>
2016-11-09 16:52:50 -1000

    Another way to throw exceptions w/ messages on Rest side.

--- src/test/HelloWorldResource.java
+++ src/test/HelloWorldResource.java
@@ -2,7 +2,9 @@ package test;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/test")
 public class HelloWorldResource {
@@ -16,10 +18,11 @@ public class HelloWorldResource {
     @GET
     @Path("testMessage")
     @Produces(MediaType.APPLICATION_JSON)
-    public TestObject getTestMessage() throws TestException {
+    public TestObject getTestMessage() throws WebApplicationException {
     	TestObject to = new TestObject(1, "james");
     	if (to != null) {
-    		throw new TestException("ggg2");
+    		TestObject test = new TestObject(33, "gorilla");
+    		throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity(test).build());
     	}
     	return to;
     }
